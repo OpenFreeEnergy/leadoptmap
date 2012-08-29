c040f5d7cb28ba5ea4aa1b22b3660b61e9cd15a0
yujiewu <yujiewu@5122b2da-35c2-0410-88f4-9c43cacc4049>
2012-08-29 17:44:28 +0000

    Enhanced the command options, and added support for graphviz drawing.

--- contribute/main.py
+++ contribute/main.py
@@ -23,7 +23,7 @@ DEBUG = False
 
 
 
-def main( mol_fnames ) :
+def main( mol_fnames, output ) :
     """
     @type  mol_fnames: C{list} of C{str}'s
     @param mol_fnames: A list of structure file names
@@ -43,7 +43,7 @@ def main( mol_fnames ) :
             print "%.3f %s" % (basic_rule.similarity( mol_id[0], mol_id[1], mcs_id = id ), KBASE.ask( id )[0].title(),)
 
     # Gets graph (`g') and clusters (`c').
-    g, c = graph.gen_graph( mcs_ids, basic_rule, simi_cutoff = 0.2, max_csize = 128, num_c2c = 1 )
+    g, c = graph.gen_graph( mcs_ids, basic_rule, simi_cutoff = 0.1, max_csize = 128, num_c2c = 1 )
 
     if (DEBUG) :
         print "%d clusters (counted as the connected components in graph):" % len( c )
@@ -57,16 +57,45 @@ def main( mol_fnames ) :
  
     l = networkx.spring_layout( g, iterations = 256, weight = "similarity", scale = 10 )
     networkx.draw_networkx( g, pos = l, with_labels = False )
-    plt.savefig( "path.png" )
+    plt.savefig( output + ".png" )
 
+    try :
+        import graphviz
+        
+        ag = networkx.to_agraph( g )
+        ag.node_attr["fixedsize"] = True
+        ag.edge_attr["penwidth" ] = 2.0
+        
+        for e in ag.nodes_iter() :
+            e.attr["label"] = str( e )[:7]
+
+        simi  = [float( e.attr["similarity"] ) for e in ag.edges()]
+        scale = 1.0 / max( simi )
+        for e in ag.edges_iter() :
+            saturation = float( e.attr["similarity"] ) * scale
+            saturation = 0.0 if (saturation < 0) else (1.0 if (saturation > 1) else saturation)
+            e.attr["color"] = "0.8396,%f,0.8" % saturation
+            if (saturation < 0.01) :
+                e.attr["style"] = "dashed"
+        ag.write( output + ".dot" )
+    except ImportError :
+        pass
+    
     
 
 if ("__main__" == __name__) :
-    argv = sys.argv
+    from optparse import OptionParser
+
+    parser = OptionParser( usage = "Usage: %prog [options] <structure-file dir>", version = "%prog v0.2" )
+    parser.add_option( "-o", "--output", metavar = "BASENAME", default = "simimap",
+                       help = "output files' base name (two files will be written: <basename>.png and <basename>.dot)" )
+
+    (opt, args) = parser.parse_args()
+
     try :
-        dir = argv[1]
+        dir = args[0]
     except IndexError :
-        print "$SCHRODINGER/run %s <%s>" % (argv[0], "structure-file dir",)
+        parser.print_help()
         sys.exit( 0 )
 
     n          = 0
@@ -82,5 +111,5 @@ if ("__main__" == __name__) :
     print "--------------------------------------------"
     print "%d files in total" % len( mol_fnames )
     if (len( mol_fnames ) > 1) :
-        main( mol_fnames[:128] )
+        main( mol_fnames, output = opt.output )
         
