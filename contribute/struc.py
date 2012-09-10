58f7f1c9f3173f7c489e4953b2156feb3b529409
yujiewu <yujiewu@5122b2da-35c2-0410-88f4-9c43cacc4049>
2012-09-10 18:58:05 +0000

    Added back the code that was dropped by accident during merging.

--- contribute/struc.py
+++ contribute/struc.py
@@ -10,6 +10,11 @@ import hashlib
 
 
 
+# Flag indicating which infrastructure we are using.
+infrastructure = None    # "schrodinger" | "oechem"
+
+
+
 class _AtomContainer( object ) :
     """
 
@@ -52,11 +57,17 @@ class Struc( object ) :
 
     """
     def __init__( self ) :
-        self.atom = _AtomContainer( self )
+        # Public attributes:
+        self.atom = None
 
         # Private attributes:
         self._id = None
 
+        
+
+    def __str__( self ) :
+        return self.title()
+    
 
         
     def _atom( self, index ) :
@@ -66,33 +77,46 @@ class Struc( object ) :
         raise NotImplementedError( "`_atom()' method not implemented by subclass" )
 
 
-    
-    def id( self ) :                                          
-        """                                                   
-        Returns the ID of this structure.                     
-        """                                                   
-        if (self._id is None) :                               
+
+    def id( self ) :
+        """
+        Returns the ID of this structure.
+        """
+        if (self._id is None) :
             self._id = hashlib.sha1( self.title() ).hexdigest()
         return self._id
 
 
-    
-    def set_id( self, id ) :                                  
-        """                                                   
-        Sets the ID for this structure.                       
-        """                                                   
+
+    def set_id( self, id ) :
+        """
+        Sets the ID for this structure.
+        """
         self._id = id
 
 
 
-    def title( self ) :
-        raise NotImplementedError( "`title()' method not implemented by subclass" )
-
+    def copy( self ) :
+        raise NotImplementedError( "`copy' method not implemented by subclass" )
 
     
-    def set_title( self, new_title ) :                        
-        raise NotImplementedError( "`set_title' method not implemented by subclass" )
 
+    def extract( self, indices ) :
+        """
+        Return a new structure object which contains the atoms of the current structure that appear in the specified list.
+        """
+        raise NotImplementedError( "`extract' method not implemented by subclass" )
+    
+    
+
+    def title( self ) :
+        raise NotImplementedError( "`title' method not implemented by subclass" )
+    
+    
+
+    def set_title( self, new_title ) :
+        raise NotImplementedError( "`set_title' method not implemented by subclass" )
+    
 
         
     def heavy_atoms( self ) :
@@ -103,11 +127,69 @@ class Struc( object ) :
 
 
 
+    def is_chiral_atom( self, atom_index ) :
+        """
+        Returns true if the atom indicated by C{atom_index} is chiral; otherwise, false.
+
+        @type  atom_index: C{int}
+        @param atom_index: Atom index
+        """
+        raise NotImplementedError( "`is_chiral_atom' method not implemented by subclass" )
+    
+    
+    
+    def chiral_atoms( self ) :
+        """
+        Returns the indices of the chiral atoms.
+
+        @rtype : C{list} of C{int}
+        @return: A list of atom indices
+        """
+        raise NotImplementedError( "`chiral_atoms' method not implemented by subclass" )
+
+
+
+    def ring_atoms( self ) :
+        """
+        Returns a set of ring atoms.
+
+        @rtype : C{set} of C{int}
+        @return: A set of atom indices
+        """
+        raise NotImplementedError( "`chiral_atoms' method not implemented by subclass" )
+
+
+
+    def bonded_atoms( self, atom_index ) :
+        """
+        Returns a list of atom indices of atoms bonded to the indicated atom.
+        
+        @type  atom_index: C{int}
+        @param atom_index: A single index or a list of indices of the atoms to be deleted
+        
+        @rtype : C{list} of C{int}
+        @return: A list of atom indices of atoms bonded to the indicated atom
+        """
+        raise NotImplementedError( "`bonded_atoms' method not implemented by subclass" )
+
+
+
     def total_charge( self ) :
         """
         Returns the total charge of the structure.
         """
         raise NotImplementedError( "`total_charge' method not implemented by subclass" )
+
+
+
+    def delete_atom( self, atom_index ) :
+        """
+        Deletes a atom.
+
+        @type  atom_index: C{int} or C{list} of C{int}
+        @param atom_index: A single index or a list of indices of the atoms to be deleted
+        """
+        raise NotImplementedError( "`delete_atoms' method not implemented by subclass" )
 
 
 
@@ -122,6 +204,205 @@ class Struc( object ) :
         raise NotImplementedError( "`write' method not implemented by subclass" )
 
     
+
+try :
+    import schrodinger.structure as structure
+    
+    class SchrodStruc( Struc ) :
+        """
+        A `Struc' subclass based on Schrodinger's infrastructure
+        """
+        def __init__( self, struc ) :
+            """
+            `struc' should be a `schrodinger.structure.Structure' object.
+            """
+            Struc.__init__( self )
+            self._struc = struc
+
+            # Public attributes:
+            self.atom = self._struc.atom
+            self.bond = self._struc.bond
+
+
+
+        def copy( self ) :
+            """
+            Returns a copy of this structure.
+            """
+            return SchrodStruc( self._struc.copy() )
+
+
+
+        def extract( self, indices ) :
+            """
+            Return a new structure object which contains the atoms of the current structure that appear in the specified list.
+            """
+            return SchrodStruc( self._struc.extract( indices, True ) )
+
+        
+        
+        def title( self ) :
+            """
+            Returns the title of this structure. (Normally title's a user-friendly description)
+            """
+            return self._struc.title
+        
+            
+            
+        def set_title( self, new_title ) :
+            """
+            Sets a new title to this structure.
+            """
+            self._struc.title = new_title
+        
+            
+            
+        def heavy_atoms( self ) :
+            """
+            Returns a list of indices of heavy atoms (viz non-hydrogen atoms).
+            """
+            ret = []
+            for e in self.atom :
+                if (e.atomic_number > 1) :
+                    ret.append( int( e ) )
+            return ret
+
+
+
+        def is_chiral_atom( self, atom_index ) :
+            """
+            Returns true if the atom indicated by C{atom_index} is chiral; otherwise, false.
+
+            @type  atom_index: C{int}
+            @param atom_index: Atom index
+            """
+            return self._struc.atom[atom_index].chirality in ["R", "S", "ANR", "ANS"]
+        
+        
+        
+        def chiral_atoms( self ) :
+            """
+            Returns the indices of the chiral atoms.
+
+            @rtype : C{list} of C{int}
+            @return: A list of atom indices
+            """
+            ret = []
+            for atom in self._struc.atom :
+                if (atom.chirality in ["R", "S", "ANR", "ANS"]) :
+                    ret.append( int( atom ) )
+            return ret
+
+
+
+        def ring_atoms( self ) :
+            """
+            Returns a set of ring atoms.
+
+            @rtype : C{set} of C{int}
+            @return: A set of atom indices
+            """
+            rings = self._struc.find_rings()
+            ret   = []
+            for e in rings :
+                ret.extend( e )
+            return set( ret )
+
+
+
+        def bonded_atoms( self, atom_index ) :
+            """
+            Returns a list of atom indices of atoms bonded to the indicated atom.
+            
+            @type  atom_index: C{int}
+            @param atom_index: A single index or a list of indices of the atoms to be deleted
+            
+            @rtype : C{list} of C{int}
+            @return: A list of atom indices of atoms bonded to the indicated atom
+            """
+            ret = []
+            for e in self._struc.atom[atom_index].bonded_atoms :
+                ret.append( int( e ) )
+            return ret
+        
+                
+        
+        def total_charge( self ) :
+            """
+            Returns the formal charge of the structure
+            """
+            return self._struc.formal_charge
+
+
+
+        def delete_atom( self, atom_index ) :
+            """
+            Deletes a atom.
+
+            @type  atom_index: C{int} or C{list} of C{int}
+            @param atom_index: A single index or a list of indices of the atoms to be deleted
+            """
+            if (not isinstance( atom_index, list )) :
+                atom_index = [atom_index,]
+            self._struc.deleteAtoms( atom_index )
+            
+            
+        
+        def write( self, filename, format = None, mode = "a" ) :
+            """
+            Writes this structure into a file in the designated format.
+
+            @type  format: C{str} or C{None}
+            @param format: If its value is C{None}, the file format is determined from the filename suffix. If specified, it
+                           must be one of the following case-sensitive strings: "pdb", "mol2", "sd", "maestro", "smiles", and
+                           "smilescsv".
+            """
+            if   ('a' == mode) :
+                if (os.path.isfile( filename )) : self._struc.append( filename, format )
+                else                            : self._struc.write ( filename, format )
+            elif ('w' == mode) :
+                self._struc.write( filename, format )
+            else :
+                raise ValueError( "Invalid value for `mode' argument: '%s', should be one of 'a' and 'w'." )
+
+            
+            
+    def read_file( filename, format = None ) :
+        """
+        Reads a .mae file and returns a list of `ScrhodStruc' objects.
+
+        @type  filename: C{str}
+        @param filename: Name of the structure file
+        @type  format  : C{str} or C{None}
+        @param format  : Specify the format of the file. It must be one of the following case-sensitive strings: "pdb", "sd",
+                         "mol2", and "maestro". If its value is C{None}, the format will be determined from extension name.
+        """
+        ret = []
+        for ct in structure.StructureReader( filename, format = format ) :
+            ret.append( SchrodStruc( ct ) )
+        return ret
+
+
+
+    def read_n_files( filenames ) :
+        """
+        `filenames' is a list of file names. The format of each file will be determined from the file's extension name. Reads
+        the files and deposits them into the `KBASE'. Returns a list of keys.
+        """
+        strucid = []
+        for fn in filenames :
+            strucs = read_file( fn )
+            for e in strucs :
+                id = KBASE.deposit( e.id(), e )
+                e.set_id( id )
+                strucid.append( id )
+        return strucid
+
+    infrastructure = "schrodinger"
+    
+except ImportError, e :
+    pass
+
 
 
 try:
@@ -286,10 +567,19 @@ try:
             strucid.append( id )
         return strucid        
 
+    infrastructure = "oechem"
+
 except ImportError, e :
-    raise e
     pass
 
+
+
+if (infrastructure is None) :
+    print "ERROR: Need either Schrodinger's or OEChem's infrastructure to run, but none is found."
+    import sys
+    sys.exit( 1 )
+
+    
 
 
 if ("__main__" == __name__) :
